b9d1e606758596a274b58c8b2bf46fdc5e13b341
skerikpa <240982@vutbr.cz>
2023-11-02 10:42:14 +0100

    lab6

---
+++ 06-i2c-oled/src/main.c
@@ -0,0 +1,50 @@
+// https://github.com/Sylaina/oled-display
+
+#include <oled.h>
+
+int main(void)
+{
+    oled_init(OLED_DISP_ON);
+    oled_clrscr();
+
+    for(int i = 0; i<50; i++)
+    {
+      oled_gotoxy(i, 0);
+      oled_putc('|');
+      oled_display();
+    }
+
+    oled_clrscr();
+
+    oled_charMode(DOUBLESIZE);
+    oled_puts("OLED disp.");
+
+    oled_charMode(NORMALSIZE);
+
+    // oled_gotoxy(x, y)
+    oled_gotoxy(0, 2);
+    oled_puts("128x64, SHH1106");
+
+    // oled_drawLine(x1, y1, x2, y2, color)
+    oled_drawLine(0, 25, 120, 25, WHITE);
+
+    oled_gotoxy(0, 4);
+    oled_puts("BPC-DE2, Brno");
+
+    oled_drawCircle(64,32,20,WHITE);
+
+    oled_puts("HELLO");
+
+    oled_flip(1);
+    oled_invert(1);
+
+
+    // Copy buffer to display RAM
+    oled_display();
+
+    while (1) {
+        ;
+    }
+
+    return 0;
+}
